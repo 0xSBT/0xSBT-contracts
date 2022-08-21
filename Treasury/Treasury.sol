7cb2594c8f7a4f1ca23147103d6a67339d5074f7
hyeonLewis <kimhyunwoo554@gmail.com>
2022-08-21 13:21:45 +0900

    add investment functions for treausry contract

--- Treasury/Treasury.sol
+++ Treasury/Treasury.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.1;
 
 import "../Interfaces/IVoting.sol";
 import "../Interfaces/ITreasury.sol";
+import "../Interfaces/IPoolRouter.sol"; // Pangea swap
+import "../Interfaces/IConcentratedLiquidityPoolFactory.sol";
+import "../Interfaces/IKSP.sol"; // Klayswap
+import "../Interfaces/IKSLP.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "https://github.com/klaytn/klaytn-contracts/blob/master/contracts/KIP/token/KIP17/extensions/IKIP17Enumerable.sol";
@@ -16,16 +20,32 @@ contract Treasury is Ownable, ITreasury {
     
     IKIP17Enumerable public governToken;
     IVoting public voteContract;
+    IPoolRouter public pangeaRouter;
+    IConcentratedLiquidityPoolFactory public pangeaPoolFactory;
+    IKSP public ksp;
 
     mapping (uint256 => Withdraw) public withdrawList;
+    mapping (address => bool) public whiteListToken;
 
-    constructor(address _governToken, address _voteContract) {
+    constructor(address _governToken, address _voteContract, address _ksp, address _pangeaPoolRouter, address _pangeaPoolFactory) {
         governToken = IKIP17Enumerable(_governToken);
         voteContract = IVoting(_voteContract);
+        ksp = IKSP(_ksp);
+        pangeaRouter = IPoolRouter(_pangeaPoolRouter);
+        pangeaPoolFactory = IConcentratedLiquidityPoolFactory(_pangeaPoolFactory);
     }
 
+    receive () payable external {}
+
+    modifier whiteList(address token) {
+        require (whiteListToken[token], "Can't use this token");
+        _;
+    }
+
+    //////////// WITHDRAW ///////////
+
     function makeVoteForWithdraw(string memory agenda, address token, uint256 amount) external onlyOwner {
-        require (IKIP7(token).balanceOf(address(this)) >= amount, "Not enough balance");
+        require (balanceOfToken(token) >= amount, "Not enough balance");
 
         uint256 _voteId = voteContract.propose(agenda);
 
@@ -67,5 +87,64 @@ contract Treasury is Ownable, ITreasury {
 
     function emergencyWithdraw(address token, address receiver, uint256 amount) external onlyOwner {
         IKIP7(token).transfer(receiver, amount);
+    }
+
+    /////////// INVESTMENT ///////////
+
+    function swapTokenUsingPangea(address inToken, address outToken, uint256 amount) public whiteList(outToken) onlyOwner {
+        require (balanceOfToken(inToken) >= amount, "Not enough token to swap");
+
+        address pool = pangeaPoolFactory.getPools(inToken, outToken, 0, 1)[0];
+
+        ExactInputSingleParams memory newParams = ExactInputSingleParams({
+            tokenIn: inToken,
+            amountIn: amount,
+            amountOutMinimum: 0,
+            pool: pool,
+            to: outToken,
+            unwrap: false
+        });
+
+        pangeaRouter.exactInputSingle(newParams);
+    }
+
+    function addLiquidityInKlayswap(address tokenA, address tokenB) public whiteList(tokenA) whiteList(tokenB) onlyOwner {
+        require (checkBalance(tokenA) || checkBalance(tokenB), "No token");
+
+        uint256 amountA = balanceOfToken(tokenA);
+        uint256 amountB = balanceOfToken(tokenB);
+
+        address pool = ksp.tokenToPool(tokenA, tokenB);
+
+        if (amountA > 0 && amountB > 0){
+            uint256 estimatedA = estimateSupply(tokenB, amountB, pool);
+            uint256 estimatedB = estimateSupply(tokenA, amountA, pool);
+
+            if (amountB >= estimatedB) {
+                swapTokenUsingPangea(tokenB, tokenA, (amountB - estimatedB).div(2));
+                IKSLP(pool).addKctLiquidity(balanceOfToken(tokenA), balanceOfToken(tokenB));
+            }
+            else {
+                swapTokenUsingPangea(tokenA, tokenB, (amountA - estimatedA).div(2));
+                IKSLP(pool).addKctLiquidity(balanceOfToken(tokenA), balanceOfToken(tokenB));
+            }
+        }
+    }
+
+    function estimateSupply(address token, uint256 amount, address pool) internal view returns (uint256) {
+        require(token == tokenA || token == tokenB);
+
+        uint256 pos = IKSLP(pool).estimatePos(token, amount);
+        uint256 neg = IKSLP(pool).estimateNeg(token, amount);
+
+        return (pos + neg)/2;
+    }
+
+    function checkBalance(address token) internal view returns (bool) {
+        return IKIP7(token).balanceOf(address(this)) > 0;
+    }
+
+    function balanceOfToken(address token) internal view returns (uint256) {
+        return IKIP7(token).balanceOf(address(this));
     }
 }
